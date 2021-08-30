dbce8d4dd5ba183f17816b1dff87b8d332cf2334
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2021-08-30 23:31:59 +0530

    Create combinationsum.java

---
+++ Day 10/combinationsum.java
@@ -0,0 +1,29 @@
+class Solution {
+ public int combinationSum(int[] arr, int tar, int idx, List<Integer> smallAns, List<List<Integer>> res) {
+        if (tar == 0) {
+            List<Integer> base = new ArrayList<>(smallAns);
+            res.add(base);
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = idx; i < arr.length; i++) {
+            if (tar - arr[i] >= 0) {
+                smallAns.add(arr[i]);
+                count += combinationSum(arr, tar - arr[i], i, smallAns, res);
+                smallAns.remove(smallAns.size() - 1);
+            }
+        }
+
+        return count;
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> smallAns = new ArrayList<>();
+
+        combinationSum(candidates, target, 0, smallAns, res);
+
+        return res;
+    }
+}
